50f35441c8ce1af8d056a3cd39f0f442e869f7d7
anan <nut999anan@hotmail.com>
2024-05-12 13:53:19 +0700

    main page almost done

--- StockMinderApp/Views/MainPageDetail.xaml.cs
+++ StockMinderApp/Views/MainPageDetail.xaml.cs
@@ -26,4 +26,33 @@ public partial class MainPageDetail : ContentPage
             NavigationHelper.ChangeFlyoutDetails(new Views.LoginPage());
         }
     }
+
+    void AddProductTapped(System.Object sender, Microsoft.Maui.Controls.TappedEventArgs e)
+    {
+        if (App.UserSession.IsLoggedIn)
+        {
+            NavigationHelper.ChangeFlyoutDetails(new Views.AddProductPage());
+
+        }
+        else
+        {
+            App.previousPage = new AddProductPage();
+            NavigationHelper.ChangeFlyoutDetails(new Views.LoginPage());
+        }
+    }
+
+
+    void ProductsTapped(System.Object sender, Microsoft.Maui.Controls.TappedEventArgs e)
+    {
+        if (App.UserSession.IsLoggedIn)
+        {
+            NavigationHelper.ChangeFlyoutDetails(new Views.ProductsPage());
+
+        }
+        else 
+        {
+            App.previousPage = new ProductsPage();
+            NavigationHelper.ChangeFlyoutDetails(new Views.LoginPage());
+        }
+    }
 }
